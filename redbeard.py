e3d25597ed0c5cd27ffa9b6f28ef5c2a32b7aeeb
Kenneth Love <kenneth@gigantuan.net>
2010-12-07 17:31:30 -0800

    working on save

--- redbeard.py
+++ redbeard.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 import redis
 app = Flask(__name__)
 app.debug = True
@@ -12,17 +12,29 @@ def index():
 
 @app.route('/key/<key>')
 def key(key):
-    if r.type(key) == 'hash':
+    rtype = r.type(key)
+    if rtype == 'hash':
         output = r.hgetall(key)
-    elif r.type(key) == 'set':
+    elif rtype == 'set':
         output = r.smembers(key)
     else:
         output = r.get(key)
     return render_template('key.html', key=key, output=output)
 
-@app.route('/info/')
-def info():
-    return render_template('info.html', info=r.info().items())
+@app.route('/key/save/<key>', methods=['POST'])
+def save(key):
+    rtype = r.type(key)
+    value = request.form['value'].strip('{}')
+    new_value = dict([kv.split(':') for kv in value])
+
+    return ', '.join(new_value.keys())
+    if rtype == 'hash':
+        #r.delete(key)
+        #r.set(key, request.form['value'])
+        #r.set(key, "{'balls':'lick them'}")
+        pass
+
+    return redirect(url_for('key', key=key))
 
 if __name__ == '__main__':
     app.run()
