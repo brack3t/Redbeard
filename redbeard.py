a6f9e1f6b34d674004dd049da498634733078840
Chris Jones <chris@brack3t.com>
2010-12-08 22:36:30 -0800

    Added a simple 1kb grid, css reset and layout with blocks. Very basic, ugly but getting better. Added a session creation method to set redis defaults. Added another method to handle getting the redis connection with the session specific data for each view that uses it. Added a form to change the redis db and a view which handles changing the redis db key in the session data. Super basic but working.

--- redbeard.py
+++ redbeard.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import session
 import redis
 import settings
 
@@ -8,23 +9,64 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.debug = True
 
-r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
-    db=settings.REDIS_DB)
+def get_redis_connection(session):
+    """ Get Redis connection with session values """
+    return redis.Redis(host=session['redis_host'], port=session['redis_port'],
+        db=session['redis_db'])
+
+def set_session_defaults(session):
+    """ Setup default session """
+    session['redis_db'] = settings.REDIS_DB
+    session['redis_host'] = settings.REDIS_HOST
+    session['redis_port'] = settings.REDIS_PORT
+
+@app.route('/logout/')
+def logout():
+    if session.has_key('redis_db'):
+        session.pop('redis_db', None)
+    return redirect(url_for('index'))
+
+@app.route('/change_db', methods=['GET', 'POST'])
+def change_db():
+    """
+    View to handle changing the redis db. Make sure val is an int
+    and withing the redis db range.
+    """
+    if request.method == 'POST':
+        try:
+            db = int(request.form['redis_db'])
+        except ValueError:
+            return redirect(url_for('index'))
+
+        if db >= 0 and db <= 9:
+            session['redis_db'] = db
+            flash('Redis DB changed to ' + str(db))
+    return redirect(url_for('index'))
+
 
 @app.route('/info/')
 def info():
     """ View for info about your redis set up. """
-    return render_template('info.html', info=r.info().items())
+    r = get_redis_connection(session)
+    info = r.info().items()
+
+    return render_template('info.html', info=info)
 
 @app.route('/')
 def index():
     """ All available keys. """
+    if not session.has_key('redis_db'):
+        set_session_defaults(session)
+    r = get_redis_connection(session)
+
     keys = r.keys()
     return render_template('index.html', keys=keys)
 
 @app.route('/key/<key>')
 def key(key):
     """ Info for the key. """
+    r = get_redis_connection(session)
+
     rtype = r.type(key)
     if rtype == 'hash':
         output = r.hgetall(key)
@@ -39,6 +81,8 @@ def key(key):
 @app.route('/key/save/<key>', methods=['POST'])
 def save(key):
     """ Update the value of a key. """
+    r = get_redis_connection(session)
+
     rtype = r.type(key)
     value = request.form['value']
 
