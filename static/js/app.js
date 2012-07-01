96ee194b8e3fadbc30f38307245eed924657910d
Kenneth Love <kenneth@gigantuan.net>
2012-07-01 11:13:20 -0700

    filter the list by type

--- static/js/app.js
+++ static/js/app.js
@@ -12,10 +12,44 @@ Redbeard.SearchTextField = Ember.TextField.extend({
     }
 });
 
+Redbeard.FilterSelectField = Ember.Select.extend({
+    content: Ember.A([
+        {type: "all", label: "All"},
+        {type: "hash", label: "Hashes"},
+        {type: "list", label: "Lists"},
+        {type: "set", label: "Sets"},
+        {type: "string", label: "Strings"},
+        {type: "zset", label: "ZSets"}
+    ]),
+    optionLabelPath: "content.label",
+    optionValuePath: "content.type"
+});
+
 Redbeard.keysController = Ember.ArrayController.create({
     content: [],
     key_name: '',
     query: '',
+    type_filter: '',
+    total_content: [],
+
+    filter_type: function() {
+        var self = this,
+            type_filter = self.get("type_filter");
+
+        if (type_filter !== null && type_filter.type !== "all") {
+            self.set("content", self.total_content);
+            var real_type_filter = type_filter.type,
+                content = self.get("content");
+
+            new_content = _.filter(content, function(key) {
+                return key.type === real_type_filter;
+            });
+            self.set("content", new_content);
+        } else {
+            self.loadKeys();
+        }
+
+    }.observes("type_filter"),
 
     filter: function() {
         var self = this,
@@ -42,6 +76,7 @@ Redbeard.keysController = Ember.ArrayController.create({
             data.forEach(function(key) {
                 self.pushObject(Redbeard.Key.create(key));
             });
+            self.set("total_content", self.content);
         });
     }
 });
